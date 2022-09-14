e855ea9b7778763092c639bfa6fa5f0b84552629
frankielan168 <frankielan168@gmail.com>
2022-09-14 21:44:45 +0800

    w02-P5: copy w02 files to demo/w02_booklist

---
+++ demo/W2_booklist/src/App_64.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App_64.css';
+
+import Booklist_64 from './components/Booklist_64';
+
+
+const App_64 = () => {
+  return <Booklist_64 />
+};
+
+export default App_64;
