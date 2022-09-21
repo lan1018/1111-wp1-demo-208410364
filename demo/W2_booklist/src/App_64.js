f74ca8e35354c6cb9183bb64955add177cb71af1
frankielan168 <frankielan168@gmail.com>
2022-09-21 21:36:44 +0800

    W03-P2: show all meals using data.js

--- demo/W2_booklist/src/App_64.js
+++ demo/W2_booklist/src/App_64.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import Menu_64 from './components/Menu_64';
+import items from './data'
 import React from 'react';
 import './App_64.css';
 
 const App_64 = () => {
+  const [menuItems, setMenuItems] = useState(items);
+
+  //console.log('menuItems', menuItems);=
   return (
     <section className="menu">
         {/* title */}
@@ -28,45 +34,10 @@ const App_64 = () => {
           </button>
         </div>
         {/* menu items */}
-        <div className="section-center">
-          <article className="menu-item">
-            <img src="/images/item-1.jpeg" alt="buttermilk" pancakes="" className="photo"/>
-            <div className='item-info'>
-              <header>
-                <h4>buttermilk pancakes</h4>
-                <h4 className="price">$15.99</h4>
-              </header>
-              <p className="item-text">
-                I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed 
-              </p>
-            </div>
-          </article>
-          <article className="menu-item">
-            <img src="/images/item-2.jpeg" alt="diner" double="" className="photo"/>
-            <div className="item-info">
-              <header>
-                <h4>diner double</h4>
-                <h4 className="price">$13.99</h4>
-              </header>
-              <p className="item-text">
-                vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats 
-              </p>
-            </div>
-          </article>
-          <article className="menu-item">
-            <img src="/images/item-3.jpeg" alt="godzilla" milkshake="" className="photo"/>
-            <div className="item-info">
-              <header>
-                <h4>godzilla milkshake</h4>
-                <h4 className="price">$6.99</h4>
-              </header>
-              <p className="item-text">
-                ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.
-              </p>
-            </div>
-          </article>
-          </div>
+        <Menu_64 items={menuItems} />
+        <script src="app.js"></script>
       </section>
+       
   );
 };
 
